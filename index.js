610d3ddc040990d1c21c59b5cf98e60e754ef04f
dsdorazio <dsdorazio@penguin>
2022-08-06 16:18:38 -0600

    First cut

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var cors = require('cors');
+const multer  = require('multer');
+const upload = multer({ dest: './public/data/uploads/' });
 require('dotenv').config()
 
 var app = express();
@@ -11,8 +13,11 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  const data = {name: req.file.originalname, type: req.file.mimetype, size: req.file.size}
+  res.status(200).json(data);
+  console.log(req.file, req.body);
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
